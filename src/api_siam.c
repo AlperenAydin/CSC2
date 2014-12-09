a4a6021b70ad670a9d8c558b3c50bb739cd2647c
AlperenAydin <aydin.alperen.bugra@gmail.com>
2014-12-09 11:02:58 +0100

    Debougage 2.0

--- src/api_siam.c
+++ src/api_siam.c
@@ -10,10 +10,45 @@ coup_jeu api_siam_tenter_introduire_nouvelle_piece_si_possible(jeu_siam* jeu,
                                                                int x,int y,
                                                                orientation_deplacement orientation)
 {
-    //coder cette fonction
-    coup_jeu coup;
-    coup_jeu_initialiser(&coup);
-    return coup;
+ 
+
+  assert(jeu!=NULL);
+ 
+  int Valid=1;
+
+  const piece_siam* piece = plateau_obtenir_piece_info(&jeu->plateau,x,y);
+  type_piece piece_du_joueur = joueur_obtenir_animal (jeu->joueur);
+
+  if(coordonnees_etre_dans_plateau(x,y)==0)//On vérifie que les coordonées sont valides
+    {
+      Valid=0;
+    }  
+  
+  if(plateau_exister_piece(&(jeu->plateau),x,y)==0){Valid=0;} //On vérifie que la pièce existe sur ce plateau
+  
+  if(joueur_etre_type_animal(jeu->joueur,piece->type)==0){Valid=0;}//On vérifie que le type du joueur correspond bien au type de la pièce
+    
+  if (plateau_modification_introduire_piece_etre_possible(&(jeu->plateau),x,y,piece_du_joueur,orientation)==1){Valid =0;}
+
+  if(Valid==1)
+    {
+      plateau_modification_introduire_piece(&(jeu->plateau),x,y,piece_du_joueur,orientation,NULL);
+      coup_jeu coup;
+      coup_jeu_initialiser(&coup);
+      coup.valide = 1;
+      printf("Le coup est valide \n");
+      return coup;
+
+    }
+  else
+    {
+      coup_jeu coup;
+      coup_jeu_initialiser(&coup);
+      coup.valide = 0;
+      printf("Le coup n'est pas valide \n");
+      return coup;
+      
+    }
 }
 
 
@@ -23,10 +58,44 @@ coup_jeu api_siam_tenter_deplacer_piece_si_possible(jeu_siam* jeu,
                                                     orientation_deplacement deplacement,
                                                     orientation_deplacement orientation)
 {
-    //coder cette fonction
-    coup_jeu coup;
-    coup_jeu_initialiser(&coup);
-    return coup;
+   
+
+  assert(jeu!=NULL);
+ 
+  int Valid=1;
+
+  const piece_siam* piece = plateau_obtenir_piece_info(&jeu->plateau,x,y);
+
+  if(coordonnees_etre_dans_plateau(x,y)==0)//On vérifie que les coordonées sont valides
+    {
+      Valid=0;
+    }  
+  
+  if(plateau_exister_piece(&(jeu->plateau),x,y)==0){Valid=0;} //On vérifie que la pièce existe sur ce plateau
+  
+  if(joueur_etre_type_animal(jeu->joueur,piece->type)==0){Valid=0;}//On vérifie que le type du joueur correspond bien au type de la pièce
+    
+  if (plateau_modification_deplacer_piece_etre_possible(&(jeu->plateau),x,y,deplacement,orientation)==1){Valid =0;}
+
+  if(Valid==1)
+    {
+      plateau_modification_deplacer_piece(&(jeu->plateau),x,y,deplacement,orientation,NULL);
+      coup_jeu coup;
+      coup_jeu_initialiser(&coup);
+      coup.valide = 1;
+      printf("Le coup est valide \n");
+      return coup;
+
+    }
+  else
+    {
+      coup_jeu coup;
+      coup_jeu_initialiser(&coup);
+      coup.valide = 0;
+      printf("Le coup n'est pas valide \n");
+      return coup;
+      
+    }
 }
 
 
