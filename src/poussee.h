d04e4cec2d4efd5f052dd6211fd76130dd4fafa3
Lesly Cotte <lesly.cotte@mastertp>
2014-12-09 11:33:39 +0100

    AJout d'un nouveau fichier poussee.h

---
+++ src/poussee.h
@@ -0,0 +1,34 @@
+/***
+* Fonction poussee_etre_valide:
+*******************************
+* Vérifie si il est possible de réaliser une poussee qui débute aux cordonées (x,y) dans l'orientation definie;
+* Note: Les coordonées (x,y) designent la premiere piece rencontree dans la poussee.
+*
+*
+*Necessite :
+* -Un pointeur non NULL vers un plateau integre non modifiable
+* - Deux coordonnes entieres (x,y) designant une piece non vide integre du plateau
+* -Une orientation de poussee designant une direction integre
+* Garantie:
+*-Un retour valant 1 si la poussee est possible
+*-Un retour valant 0 sinon
+*
+*/
+
+
+/**
+*Fonction poussee_realiser:
+***************************
+*   Realise une poussee sur un plateau en supposant que celle-ci est réalisable.
+*
+* Necessite:
+*-Un pointeur non NULL vers un plateau integre modifiable
+*-Deux coordonnees entieres (x,y) designant une piece non vide integre du plateau.
+*-Un type d'animal à déplacer.
+*-Une orientation de poussee designant une direction integre.
+*-Un pointeur non NULL vers une condition de victoire modifiable.
+*
+*Garantie:
+*-Un plateau integre mis a jour en ayant la poussee
+*
+*/
