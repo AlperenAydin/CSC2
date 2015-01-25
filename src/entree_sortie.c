b7e1931e45018a990bc74ca1f622d8d8517c85bb
AlperenAydin <aydin.alperen.bugra@gmail.com>
2015-01-25 15:37:17 +0100

    Correction de entree_sortie_lire_jeu_fichier

--- src/entree_sortie.c
+++ src/entree_sortie.c
@@ -23,7 +23,7 @@
 
 #include "entree_sortie.h"
 
-
+#include <unistd.h>
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
@@ -113,10 +113,12 @@ void entree_sortie_lire_jeu_fichier(const char* filename,jeu_siam* jeu)
   file = fopen(filename, "r"); // On ouvre le ficihier et on le lit;
   
   // Lecture de joueur;
-  int joueur;
+  int joueur=1;
   char nom [10];
-  fscanf(file, "joueur %d (%str) \n",&joueur,nom);
+  int x = fscanf(file,"joueur %d (%8s)\n ",&joueur,nom);
+  printf("%d joueur %d (%s)\n ",x,joueur,nom);
   jeu->joueur = joueur;
+
 
   // Lecture du plateau;
   int i,j;
@@ -126,42 +128,34 @@ void entree_sortie_lire_jeu_fichier(const char* filename,jeu_siam* jeu)
   int nb_collones = 5;
   int nb_lignes   = 5;
   
+
+
   for(i=nb_lignes-1;i>=0;i--)
     {
-      fscanf(file,"[%d] ",&i); // trouve le lignes
-	
-      for(j=0;j<nb_collones;i++)
-	{
-	  char str[3];
-	  fscanf(file,"%s |",str);// On recupere le piece
+      
+      fscanf(file,
+	     "[%d] "
+	     ,&i);
+      //Trouver la ligne
+      // N'est pas exactement necessaire
 
-	  plateau.piece[i][j] = piece_correspondre_nom_cours(str); // On cherche le piece correspondant
-	  
+      for(j=0;j<nb_collones;j++)
+  	{
+	  char str_piece[3];
+
+	  fscanf(file,"%3s |",str_piece);
+	  //On prend le trois charactere qui forme notre piece
+
+	  plateau.piece[j][i] = piece_correspondre_nom_cours(str_piece);
 	}
-      fscanf(file,"\n");
+
+      fscanf(file,"\n"); 
+      // Nouvelle ligne
     }
 
   jeu->plateau = plateau;
 
   assert(jeu_etre_integre(jeu)==1);
-  
-  //Le code originale
-    /* const char* _0x10="Joueur 0 (elephant)",*_0x01="Joueur 1 (rhinoceros)"; */
-    /* const char* ___="[%d] %s | %s | %s | %s | %s |",*_0x00=filename; */
-    /* jeu_siam* __=jeu,_;int *_00x0=&_.joueur; */
-    /* piece_siam (*_0_)(const char*)=piece_correspondre_nom_cours; */
-    /* size_t (*_0x50)(const char*)=strlen;piece_siam* _00x02=*_.plateau.piece,*_0x02,_0x03; */
-    /* int _00x00_,_10x01_=0x00;char _0x100[0x100],_01x10_[0x05][0x100]; */
-    /* const char* _for="Erreur ouverture fichier %s\n",*_jeu="Erreur lecture numero de ligne",*_while="Jeu invalide lecture fichier %s\n"; */
-    /* jeu_initialiser(&_);FILE *_0x100_=fopen(_0x00,"r");assert(_0x100_!=0x00);if(_0x100_==0x00) */
-    /* {printf(_for,_0x00);exit(1);}while(fgets(_0x100,0x100,_0x100_)!=0){_00x00_=0x00; */
-    /* if(_0x50(_0x100)>=011&&strncmp(_0x100,_0x10,0x13)==0x00){*_00x0=0x00; */
-    /* }else if(_0x50(_0x100)>=011&&strncmp(_0x100,_0x01,0x13) */
-    /* ==0x00){*_00x0=0x01;}else if(sscanf(_0x100,___,&_00x00_, */
-    /* 0x00[_01x10_],0x01 [_01x10_],0x02[_01x10_],0x03[_01x10_],0x04[_01x10_]) */
-    /* ==0x06&&0x08>0x02){if(_00x00_<0x00||_00x00_>0x04){puts(_jeu);abort();}for(_10x01_=0x00; */
-    /* _10x01_<0x05;_10x01_+=0x01){_0x02=&(0x05*_10x01_+_00x00_)[_00x02];_0x03=_0_(_10x01_[_01x10_]); */
-    /* *_0x02=_0x03;}}}if(jeu_etre_integre(&_))*__=_;else printf(_while,_0x00); */
 }
 
 
@@ -197,41 +191,35 @@ void entree_sortie_ecrire_plateau_pointeur_fichier(FILE* identifiant,const plate
   
   for (i = nb_collones-1; i>=0;i--)
     {
-      fprintf(identifiant, "[%i] ",i);
+      fprintf(identifiant, "[%i]",i);
 
       for(j=0; j<nb_lignes; j++)
 	{
-	  piece_siam piece = plateau -> piece [i][j];
+	  piece_siam piece = plateau -> piece [j][i];
 	  const char *str=type_nommer_nom_cours(piece.type);
+	  //On a une case vide
 	  if(*str == '*')
 	    {	
-	      fprintf(identifiant,"%str |",str);
+	      fprintf(identifiant," %s |",str);
 	    }
+	  //On a une rocher
+	  else if(*str == 'R')
+	    {
+	      fprintf(identifiant," RRR |");
+	    }
+	  // Sinon, on a une piece
 	  else 
 	    {
 	      char t =*str;
 	      const char *c=orientation_nommer_nom_cours(piece.orientation);
-	      fprintf(identifiant,"%c - %c |",t,*c);
+	      fprintf(identifiant," %c-%c |",t,*c);
 		
-	    }
-	  fprintf(identifiant,"\n");
-	  
+	    }	  
 	}
+      fprintf(identifiant,"\n");
     }
-
-  /* const char* (*_00x00_)(type_piece)=type_nommer_nom_cours,*(*_01)(orientation_deplacement)=orientation_nommer_nom_cours; */
-  /* FILE* _=identifiant; */
-  /* int _0x00, */
-  /* (*__)(FILE*,const char*,...)=fprintf,_0x01,_0x20,_0x11; */
-  /* const piece_siam* _0x10,*_0x02=*plateau->piece; */
-  /* for(_0x20  =0x00;_0x20<0x10-0x05;_0x20+=0x01) */
-  /*   {if(_0x20==0x02){for(_0x00=0x04;_0x00>=0x00;--_0x00) */
-  /* 	  {__(_,"[%d] ",_0x00);for(_0x01=0x00;_0x01<010-03&&05<0x080;++_0x01) */
-  /* 				 {_0x10=&(0x05*_0x01+_0x00)[_0x02];for(_0x11=0;_0x11<0x04+0x04;++_0x11) */
-  /* 								     {if(_0x11==0x05){__(_,"%s",_00x00_(_0x10->type)); */
-  /* 									 if(_0x10->type==0x00 || _0x10->type==0x01)__(_,"-%s",_01(_0x10->orientation)); */
-  /* 								       }}__(_," | ");}__(_,"\n");}}}__(_,"    ");for(_0x01=0;_0x01<0x05;++_0x01){ */
-  /*   __(_,"[%d]   ",_0x01);}__(_,"\n"); */
+  
+  fprintf(identifiant,"    [0]   [1]   [2]   [3]   [4]  \n ");
 }
 
 
